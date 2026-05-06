e7d42a2a1d0b541ddf86b4d348e350cc3ce74919
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-06 00:26:01 -0400

    Creating Hero

---
+++ src/Component/Hero.tsx
@@ -0,0 +1,10 @@
+
+export function Hero() {
+
+    return (
+        <div className="hero">
+            <h1>Welcome to ImBetter</h1>
+            <p>Source for self-improvement articles and tips.</p>
+        </div>
+    )
+}
