8f61d0e64865ed67f8608d5cf5f1792645e58d12
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-06 00:08:29 -0400

    Creating

--- src/Component/Navbar.tsx
+++ src/Component/Navbar.tsx
@@ -1,1 +1,19 @@
-import 
+
+
+export function Navbar() {
+    
+    
+    return (
+        <nav>
+            <h1>ImBetter</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Articles</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+
+export default Navbar
