31dffafa44e02a53b88734199773e2b329bdbe4d
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-06 00:28:19 -0400

    Creating comments to org

--- src/Component/Hero.tsx
+++ src/Component/Hero.tsx
@@ -4,7 +4,8 @@ export function Hero() {
     return (
         <div className="hero">
             <h1>Welcome to ImBetter</h1>
-            <p>Source for self-improvement articles and tips.</p>
+            <p>Source for self-improvement articles and tips.</p>       
+            
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Navbar from '../Component/Navbar'
 import ArticleList from '../Component/ArticleList'
 import SearchBar from '../Component/SearchBar'
+import { Hero } from '../Component/Hero'
 
 export function Home() {
     return (
         <div>
             <Navbar />
+            <Hero />
             <SearchBar />
             <ArticleList />
         </div>
