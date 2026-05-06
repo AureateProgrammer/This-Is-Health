1398f69283db59171195b53b57159669f8d778f4
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-06 00:32:33 -0400

    styling

--- src/Component/Hero.tsx
+++ src/Component/Hero.tsx
@@ -3,8 +3,9 @@ export function Hero() {
 
     return (
         <div className="hero">
-            <h1>Welcome to ImBetter</h1>
-            <p>Source for self-improvement articles and tips.</p>       
+            <h1>Welcome, <span id = 'h1hero'>Become the best version of yourself</span></h1>
+            <p>Source for self-improvement articles and tips.</p> 
+            <button>Get Started</button>      
             
         </div>
     )
