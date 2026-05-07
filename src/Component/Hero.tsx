6f7553dd7f3bb350b2582bcbf51c617b35cdfcb2
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-07 13:06:52 -0400

    formatting

--- src/Component/Hero.tsx
+++ src/Component/Hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
         <div className="hero">
             <h1>Welcome, <span id = 'h1hero'>Become the best version of yourself</span></h1>
             <p>Source for self-improvement articles and tips.</p> 
-            <button>Get Started</button>      
+            <button>Get Started</button>
             
         </div>
     )
