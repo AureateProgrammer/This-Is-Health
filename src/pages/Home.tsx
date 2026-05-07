bec681e22962c255b55eab6e4334c1c6f1a136cc
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-07 12:13:56 -0400

    Putting live chat box on Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,15 @@ import Navbar from '../Component/Navbar'
 import ArticleList from '../Component/ArticleList'
 import SearchBar from '../Component/SearchBar'
 import { Hero } from '../Component/Hero'
+import {LiveChatBox} from '../Component/LiveChat'
 import { ArticleCard } from '../Component/ArticleCard'
-
 export function Home() {
     return (
         <div>
             <Navbar />
             <Hero />
             <ArticleList />
+            <LiveChatBox />
         </div>
     )
 }
