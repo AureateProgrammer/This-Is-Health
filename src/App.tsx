2e739774f11109f3ec99923f380b4902c4459f2c
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-07 12:24:49 -0400

    Adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import ArticleList from './Component/ArticleList'
 import Home from './pages/Home'
 import './App.css'
-
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import { Socialize } from './pages/Socialize'
 function App() {
 
   return (
-    <>
-    <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/socialize" element={<Socialize />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
