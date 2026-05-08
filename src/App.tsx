96930a8c77200a776ada1eb3e12c21ac9f637075
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-08 08:31:58 -0400

    Deleting socialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Home from './pages/Home'
 import './App.css'
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
-import { Socialize } from './pages/Socialize'
+// import { Socialize } from './pages/Socialize'
 function App() {
 
   return (
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/socialize" element={<Socialize />} />
+        {/* <Route path="/socialize" element={<Socialize />} /> */}
       </Routes>
     </BrowserRouter>
   )
